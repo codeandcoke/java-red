cc98d44400b2278b75f0d847b192a5e99e87f64e
Santiago Faci <santiago.faci@gmail.com>
2014-02-03 20:10:51 +0100

    Update JConecta.java

--- Chat v4/ClienteChat/src/org/sfsoft/clientechat/gui/JConecta.java
+++ Chat v4/ClienteChat/src/org/sfsoft/clientechat/gui/JConecta.java
@@ -11,6 +11,9 @@ import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+/**
+* @author Santiago Faci
+*/
 public class JConecta extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
